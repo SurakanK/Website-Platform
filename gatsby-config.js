2a4ed8f5ff206c30b9eca97632efc8ea5f0f864f
Surakan Kasurong <>
2023-04-14 21:02:27 +0700

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,7 @@ module.exports = {
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        disableVendorPrefixes: true,
-        displayName: false,
-      },
-    },
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-material-ui",
     "gatsby-plugin-react-helmet",
     {
